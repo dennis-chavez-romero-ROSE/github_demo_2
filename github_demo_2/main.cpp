3ee832a71b86ba835cdfac6fe1c58bb1c9014b27
Alexander Herber <alexander.herber@rose-simulation.com>
2023-08-02 10:04:11 +0200

    I finished my part with the printing of the numbers 10 - 20 ~Alex

--- github_demo_2/main.cpp
+++ github_demo_2/main.cpp
@@ -1,9 +1,16 @@
 // Your First C++ Program
 
 #include <iostream>
+#include <windows.h>
 
-int main() {
-    std::cout << "Hello World!"; // this is a merge conflict
-    std::cout << "Hello Metaverse!";
+int main() 
+{
+    std::cout << "Hello World!\n"; // this is a merge conflict
+    std::cout << "Hello Metaverse!\n";
+
+    for (byte i = 10; i < 21; i++)
+    {
+        printf_s("%d\n",i);
+    }
     return 0;
 }
