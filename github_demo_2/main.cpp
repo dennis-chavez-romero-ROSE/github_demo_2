1cc19e20b4c0cc4d71981bbb2f7e0d86ab8aa378
Jakob Fischer <jakob.fischer@rose-simulation.com>
2023-08-02 09:58:44 +0200

    Jakob counter

--- github_demo_2/main.cpp
+++ github_demo_2/main.cpp
@@ -2,9 +2,16 @@
 
 #include <iostream>
 
+void jakobCounter()
+{
+    for (int i = 0; i < 11; i++)
+        std::cout << "\nJakob: " << i;
+}
+
 int main() {
     std::cout << "Hello World!"; // this is a merge conflict
     std::cout << "Hello Metaverse!"; 
     std::cout << "this is an insane change"; 
+    jakobCounter();
     return 0;
 }
