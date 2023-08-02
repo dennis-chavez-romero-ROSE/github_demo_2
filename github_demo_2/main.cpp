4c3ac9bc374625b72c7c7734cec6f0b4d20e837c
Jakob Fischer <Jakob.Fischer@ROSESimulation365.onmicrosoft.com>
2023-08-02 09:47:08 +0200

    added bye text

--- github_demo_2/main.cpp
+++ github_demo_2/main.cpp
@@ -4,6 +4,7 @@
 
 int main() {
     std::cout << "Hello World!"; // this is a merge conflict
-    std::cout << "Hello Metaverse!";
+    std::cout << "Hello Metaverse!"; 
+    std::cout << "bye"; 
     return 0;
 }
