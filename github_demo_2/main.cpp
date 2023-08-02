55233ac91d134a64582fc1e85943496804b58c92
Dennis Chavez Romero <dennis.chavez-romero@rose-simulation.com>
2023-08-02 09:57:47 +0200

    Implemented function to print numbers from 20 to 30.

--- github_demo_2/main.cpp
+++ github_demo_2/main.cpp
@@ -2,9 +2,22 @@
 
 #include <iostream>
 
+void print_nums() {
+
+    for (int i = 19; i <= 30; i++)
+    {
+        std::cout << i << std::endl;
+    }
+
+}
+
 int main() {
     std::cout << "Hello World!"; // this is a merge conflict
     std::cout << "Hello Metaverse!"; 
     std::cout << "this is an insane change"; 
+    
+    print_nums();
+    
     return 0;
 }
+
